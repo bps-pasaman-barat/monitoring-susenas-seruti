4365d4ae8136d294b46b6547ce31aa9fb88430a5
ren-zi-fa <renzifebriandika923@gmail.com>
2026-01-20 13:41:58 +0700

    feat: add transition on tab

--- app/(protected)/_components/SerutiPage.tsx
+++ app/(protected)/_components/SerutiPage.tsx
@@ -1,27 +1,63 @@
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+"use client";
+
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
 import FormSerutiEntri from "./FormSerutiEntri";
 import FormSerutiMasuk from "./FormSerutiMasuk";
 export default function SerutiPage() {
+  const [activeTab, setActiveTab] = useState("entri");
+  const slideVariants = {
+    initial: { opacity: 0, x: -40 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 40 },
+  };
+
   return (
-    <>
-      <Tabs className="flex w-full flex-row items-center" defaultValue="entri">
-        <TabsList className="flex h-40 flex-col bg-blue-300">
-          <TabsTrigger className="w-full justify-start uppercase" value="entri">
-            Dokumen entri
-          </TabsTrigger>
-          <TabsTrigger className="w-full justify-start uppercase" value="masuk">
-            Dokumen Masuk
-          </TabsTrigger>
-        </TabsList>
-        <div className="flex-1">
-          <TabsContent value="entri" className=" mx-auto">
-            <FormSerutiEntri />
-          </TabsContent>
-          <TabsContent value="masuk" className=" mx-auto">
-            <FormSerutiMasuk />
-          </TabsContent>
-        </div>
-      </Tabs>
-    </>
+    <Tabs
+      value={activeTab}
+      onValueChange={setActiveTab}
+      className="flex w-full flex-row  items-center"
+    >
+      <TabsList className="flex h-40 flex-col bg-blue-300">
+        <TabsTrigger className="w-full justify-start uppercase" value="entri">
+          Dokumen Entri
+        </TabsTrigger>
+        <TabsTrigger className="w-full justify-start uppercase" value="masuk">
+          Dokumen Masuk
+        </TabsTrigger>
+      </TabsList>
+
+      <div className="relative flex-1 overflow-hidden ">
+        <AnimatePresence mode="wait">
+          {activeTab === "entri" && (
+            <motion.div
+              key="entri"
+              variants={slideVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              transition={{ duration: 0.3, ease: "easeOut" }}
+            >
+              <FormSerutiEntri />
+            </motion.div>
+          )}
+
+          {activeTab === "masuk" && (
+            <motion.div
+              key="masuk"
+              variants={slideVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              transition={{ duration: 0.3, ease: "easeOut" }}
+            >
+              <FormSerutiMasuk />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </Tabs>
   );
 }
--- app/(protected)/_components/susenasPage.tsx
+++ app/(protected)/_components/susenasPage.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { AnimatePresence, motion } from "framer-motion";
 import FormSesunasEntri from "./FormSusenasEntri";
 import FormSesunasMasuk from "./FormSusenasMasuk";
+import { useState } from "react";
 
 export default function SesunasPage() {
+  const [activeTab, setActiveTab] = useState("entri");
+  const slideVariants = {
+    initial: { opacity: 0, x: -40 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 40 },
+  };
   return (
     <>
-      <Tabs className="flex w-full flex-row items-center " defaultValue="entri">
+      <Tabs
+        value={activeTab}
+        onValueChange={setActiveTab}
+        className="flex w-full flex-row items-center "
+      >
         <TabsList className="flex h-40 flex-col border bg-blue-300 ">
           <TabsTrigger className="w-full justify-start uppercase" value="entri">
             Dokumen entri
@@ -14,13 +28,34 @@ export default function SesunasPage() {
             Dokumen Masuk
           </TabsTrigger>
         </TabsList>
-        <div className="flex-1 ">
-          <TabsContent value="entri" className="mx-auto">
-            <FormSesunasEntri />
-          </TabsContent>
-          <TabsContent value="masuk" className="mx-auto">
-            <FormSesunasMasuk />
-          </TabsContent>
+        <div className="relative flex-1 overflow-hidden ">
+          <AnimatePresence mode="wait">
+            {activeTab === "entri" && (
+              <motion.div
+                key="entri"
+                variants={slideVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ duration: 0.3, ease: "easeOut" }}
+              >
+                <FormSesunasEntri />
+              </motion.div>
+            )}
+
+            {activeTab === "masuk" && (
+              <motion.div
+                key="masuk"
+                variants={slideVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ duration: 0.3, ease: "easeOut" }}
+              >
+                <FormSesunasMasuk />
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </Tabs>
     </>
