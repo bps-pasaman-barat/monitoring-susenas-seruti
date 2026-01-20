3d2e60484b25691d972239fb4c74e19e6ede484e
ren-zi-fa <renzifebriandika923@gmail.com>
2026-01-20 09:21:41 +0700

    change:title

--- app/detail/susenas-masuk/[kecamatan]/page.tsx
+++ app/detail/susenas-masuk/[kecamatan]/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Susenas per Kecamatan",
+  title: "Susenas masuk",
 };
 
 import ButtonBack from "@/components/boilerplate/ButtonBack";
